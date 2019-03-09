78bf095c1e3287d3836cef7318c9294d678acfaf
Max5300 <47719541+Max5300@users.noreply.github.com>
2019-03-09 17:10:49 +0300

    Add files via upload

---
+++ game_num.py
@@ -0,0 +1,20 @@
+from random import randint
+x = randint(0, 21)
+attempts = 5
+print(f"Комьютер загадал число.\nУ вас есть {attempts} попытки(-ок). Удачи!")
+for i in range (attempts):
+    y = input("Попробуйте угадать: ")
+    if y.lower() == "выход":
+        break
+    else:
+        y = int(y)
+        if i == attempts - 1 and y != x:
+            print(f"Game over!\nЧисло: {x}")
+        else:
+            if y > x:
+                print("Попробуйте число меньше!")
+            elif y < x:
+                print("Попробуйте число больше!")
+            else:
+                print("Победа!")
+                break
